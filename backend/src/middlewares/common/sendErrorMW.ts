6007206c7a7e468be0c487596f71dbcf08cf5a48
Máté Tallósi <tallosi.mate@simonyi.bme.hu>
2024-02-16 22:00:30 +0100

    Add error logging and update error handling in sendErrorMW.ts

--- backend/src/middlewares/common/sendErrorMW.ts
+++ backend/src/middlewares/common/sendErrorMW.ts
@@ -5,6 +5,10 @@ import { APIResponse, APIError } from '~/@types'
 
 export const sendErrorMW = () => {
     return (err: unknown, req: Request, res: Response, next: NextFunction) => {
+        // Log error
+        console.error(err)
+
+        // Default status and response
         let status = 500
         const response: APIResponse = {
             suceess: false,
@@ -13,12 +17,14 @@ export const sendErrorMW = () => {
             data: {},
         }
 
+        // If error is an DatabaseError
         if (err instanceof DatabaseError) {
             status = 500
             response.message = err.message
             response.type = 'DATABASE_ERROR'
         }
 
+        // If error is an APIError
         if (err instanceof APIError) {
             status = err.status
             response.message = err.message
