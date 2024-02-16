c9694c8a756bab8acbb3c694dbedabfed5a9ef99
Máté Tallósi <tallosi.mate@simonyi.bme.hu>
2024-02-16 14:05:59 +0100

    Add database configuration and query

---
+++ backend/src/config/db.ts
@@ -0,0 +1,14 @@
+import { Pool } from 'pg'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const pool = new Pool({
+	user: process.env.DB_USER || 'postgres',
+	password: process.env.DB_PASSWORD || 'password',
+	host: process.env.DB_HOST || 'localhost',
+	port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+	database: process.env.DB_DATABASE || 'personal_finance_tracker',
+})
+
+export default pool
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+import db from '~/config/db'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
 
 const app = express()
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World')
+app.get('/', async (req: Request, res: Response) => {
+	const { rows } = await db.query('SELECT NOW()')
+
+	res.send('Now: ' + rows[0].now)
 })
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT} (http://localhost:${PORT})`)
 })
