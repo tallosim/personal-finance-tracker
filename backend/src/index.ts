555150496db1e25c0162d119655f7dbc6fc46ae6
Máté Tallósi <tallosi.mate@simonyi.bme.hu>
2024-02-16 13:49:17 +0100

    Add Prettier and ESLint configurations

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,11 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express, { Request, Response } from 'express'
 
 const app = express()
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-	res.send('Hello World')
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World')
 })
 
 app.listen(3000, () => {
-	console.log('Server is running on port 3000')
+  console.log('Server is running on port 3000')
 })
