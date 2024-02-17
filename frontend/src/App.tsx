c501beacc308cd020b6906543c5df84020cd0245
Máté Tallósi <tallosi.mate@simonyi.bme.hu>
2024-02-17 19:16:28 +0100

    Add Prettier and ESLint configurations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 const App = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
-};
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
 
-export default App;
+export default App
