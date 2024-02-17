8beb533a1ac2a895be6f2c8c6c7c2a766ffe9662
Máté Tallósi <tallosi.mate@simonyi.bme.hu>
2024-02-17 21:08:19 +0100

    Add signup page and update login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 
 import LoginPage from 'pages/LoginPage'
+import SignupPage from 'pages/SignupPage'
 
 const App = () => {
     return (
         <Routes>
             <Route path='/login' element={<LoginPage />} />
+            <Route path='/signup' element={<SignupPage />} />
             <Route path='/' element={<Navigate to='/login' />} />
         </Routes>
     )
--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,14 +1,18 @@
 import { Button, Container, FormControl, FormLabel, Heading, Input, Link, Stack, Text } from '@chakra-ui/react'
 
-export const LoginPage = () => (
+export const SignupPage = () => (
     <Container maxW='md' py={{ base: '12', md: '24' }}>
         <Stack spacing='8'>
             <Stack spacing={{ base: '2', md: '3' }} textAlign='center'>
                 <Heading size={{ base: 'xs', md: 'sm' }}>Personal Finance Tracker</Heading>
-                <Text color='fg.muted'>Log in to your account</Text>
+                <Text color='fg.muted'>Create an account</Text>
             </Stack>
             <Stack spacing='6'>
                 <Stack spacing='5'>
+                    <FormControl>
+                        <FormLabel htmlFor='email'>Full Name</FormLabel>
+                        <Input id='fullname' placeholder='Enter your full name' type='text' />
+                    </FormControl>
                     <FormControl>
                         <FormLabel htmlFor='email'>Email</FormLabel>
                         <Input id='email' placeholder='Enter your email' type='email' />
@@ -17,14 +21,18 @@ export const LoginPage = () => (
                         <FormLabel htmlFor='password'>Password</FormLabel>
                         <Input id='password' placeholder='********' type='password' />
                     </FormControl>
+                    <FormControl>
+                        <FormLabel htmlFor='password'>Password Repeat</FormLabel>
+                        <Input id='password-repeat' placeholder='********' type='password' />
+                    </FormControl>
                 </Stack>
-                <Button>Sign in</Button>
+                <Button>Create account</Button>
             </Stack>
             <Text textStyle='sm' color='fg.muted' textAlign='center'>
-                Don't have an account? <Link href='/signup'> Sign up</Link>
+                Already have an account? <Link href='/login'> Log in</Link>
             </Text>
         </Stack>
     </Container>
 )
 
-export default LoginPage
+export default SignupPage
