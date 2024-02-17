09bf09bbeb83fd7711b4cd820bcf378097fb9099
Máté Tallósi <tallosi.mate@simonyi.bme.hu>
2024-02-17 19:48:37 +0100

    Add Chakra UI Pro theme and customize theme

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '@chakra-ui/pro-theme'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 import App from './App'
+
+const proTheme = extendTheme(theme)
+const customTheme = extendTheme({
+    colors: {
+        ...proTheme.colors,
+        accentColor: 'gray',
+        brand: proTheme.colors.blue,
+    },
+}, proTheme)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
-            <ChakraProvider>
+            <ChakraProvider theme={customTheme}>
                 <App />
             </ChakraProvider>
         </BrowserRouter>
