dc308354e146a88e7bca6306df1ee23ea70146c8
Máté Tallósi <tallosi.mate@simonyi.bme.hu>
2024-02-18 11:13:27 +0100

    Add formik package and its dependencies

--- frontend/src/components/TransactionModal.tsx
+++ frontend/src/components/TransactionModal.tsx
@@ -18,6 +18,7 @@ import {
     NumberInput,
     NumberInputField,
 } from '@chakra-ui/react'
+import { useFormik } from 'formik'
 
 import { Transaction, Category } from '@types'
 
@@ -29,6 +30,40 @@ type TransactionModalProps = {
 }
 
 export const TransactionEditModal = ({ transaction, categories, isOpen, onClose }: TransactionModalProps) => {
+    const isEdit = transaction && isOpen
+    const initialValues = {
+        type: isEdit ? transaction.type : 'expense',
+        description: isEdit ? transaction.description : '',
+        amount: isEdit ? transaction.amount.toString() : '0.00',
+        categoryId: isEdit ? transaction.categoryId : '',
+        occurredAt: isEdit ? transaction.occurredAt.toISOString().slice(0, 16) : '',
+    }
+
+    const { values, handleChange, handleSubmit, setFieldValue } = useFormik({
+        enableReinitialize: true,
+        initialValues,
+        onSubmit: values => {
+            const newTransaction: Omit<Transaction, 'id' | 'userId' | 'updatedAt'> = {
+                type: values.type,
+                description: values.description,
+                amount: parseFloat(values.amount),
+                categoryId: values.categoryId,
+                occurredAt: new Date(values.occurredAt),
+            }
+            console.log(newTransaction)
+            // TODO: Implement the logic to save the transaction
+
+            onClose()
+        },
+    })
+
+    const handleNumberInputChange = (valueAsString: string, field: string) => {
+        const floatRegex = /^\+?\d*\.?\d{0,2}$/
+        if (floatRegex.test(valueAsString)) {
+            setFieldValue(field, valueAsString)
+        }
+    }
+
     return (
         <Modal isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
@@ -39,7 +74,13 @@ export const TransactionEditModal = ({ transaction, categories, isOpen, onClose 
                     <Stack spacing={4}>
                         <FormControl isRequired>
                             <FormLabel>Transaction type</FormLabel>
-                            <RadioGroup defaultValue='expense'>
+                            <RadioGroup
+                                id='type'
+                                name='type'
+                                defaultValue='expense'
+                                value={values.type}
+                                onChange={value => setFieldValue('type', value)}
+                            >
                                 <HStack spacing='24px'>
                                     <Radio value='expense'>Expense</Radio>
                                     <Radio value='income'>Income</Radio>
@@ -48,17 +89,36 @@ export const TransactionEditModal = ({ transaction, categories, isOpen, onClose 
                         </FormControl>
                         <FormControl isRequired>
                             <FormLabel>Description</FormLabel>
-                            <Input placeholder='Description' />
+                            <Input
+                                id='description'
+                                name='description'
+                                placeholder='Description'
+                                value={values.description}
+                                onChange={handleChange}
+                            />
                         </FormControl>
                         <FormControl isRequired>
                             <FormLabel>Amount (DKK)</FormLabel>
-                            <NumberInput min={0} precision={2} defaultValue={0}>
+                            <NumberInput
+                                id='amount'
+                                name='amount'
+                                min={0}
+                                precision={2}
+                                value={values.amount}
+                                onChange={valueAsString => handleNumberInputChange(valueAsString, 'amount')}
+                            >
                                 <NumberInputField />
                             </NumberInput>
                         </FormControl>
                         <FormControl isRequired>
                             <FormLabel>Category</FormLabel>
-                            <Select placeholder='Select category'>
+                            <Select
+                                id='categoryId'
+                                name='categoryId'
+                                placeholder='Select category'
+                                value={values.categoryId}
+                                onChange={handleChange}
+                            >
                                 {categories
                                     .sort((a, b) => a.sequence - b.sequence)
                                     .map(category => (
@@ -70,13 +130,18 @@ export const TransactionEditModal = ({ transaction, categories, isOpen, onClose 
                         </FormControl>
                         <FormControl isRequired>
                             <FormLabel>Transaction date</FormLabel>
-                            <Input type='datetime-local' />
+                            <Input
+                                id='occurredAt'
+                                name='occurredAt'
+                                type='datetime-local'
+                                value={values.occurredAt}
+                                onChange={handleChange}
+                            />
                         </FormControl>
                     </Stack>
                 </ModalBody>
-
                 <ModalFooter>
-                    <Button colorScheme='blue' mr={3}>
+                    <Button colorScheme='blue' mr={3} onClick={() => handleSubmit()}>
                         Save
                     </Button>
                     <Button onClick={onClose}>Cancel</Button>
